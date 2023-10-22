1c6e806089195c6f44d4fc6eab197373422aaa79
Naoures <nawreswelhazi@hotmail.com>
2023-10-23 00:55:41 +0200

    popup conx responsive + correction de son design

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var btnPopup = document.getElementById("btnPopup");
-var overlay = document.getElementById("wrapper"); //le popup de conx
-var body = document.body;
-//var btnClose= document.getElementById('btnClose');
+var overlay = (document.getElementsByClassName("wrapper"))[0]; //le popup de conx
+var btnClose= document.getElementById('btnClose');
 
 btnPopup.addEventListener('click', openModal);
 
 function openModal(){
-    overlay.style.display = 'block';
-    body.style.filter = "brightness(50%)";
+    overlay.style.display = 'flex';
 }
