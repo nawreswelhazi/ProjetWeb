57e5edfa6569ce8d4098af4756e2d3dd51761655
Naoures <nawreswelhazi@hotmail.com>
2023-10-23 01:42:44 +0200

    modif popup

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var btnPopup = document.getElementById("btnPopup");
-var overlay = (document.getElementsByClassName("wrapper"))[0]; //le popup de conx
+var popup = (document.getElementsByClassName("wrapper"))[0]; //le popup de conx
+var overlay = document.getElementById("overlay");
 var btnClose= document.getElementById('btnClose');
 
 btnPopup.addEventListener('click', openModal);
 
 function openModal(){
-    overlay.style.display = 'flex';
+    popup.style.display = 'block';
+    overlay.style.display = 'block';
 }
