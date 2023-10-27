8ea9260b51bae97eda110bd659d3b4f73251aed7
ayalaarif <larrifaya@gmail.com>
2023-10-27 03:40:47 +0200

    nav

--- include/nav.php
+++ include/nav.php
@@ -1,9 +1,13 @@
+<?php 
+session_start();  ?>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <!-- google fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Righteous&display=swap" rel="stylesheet"> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
 
 
 <nav class="navbar navbar-expand-lg  pb-3">
