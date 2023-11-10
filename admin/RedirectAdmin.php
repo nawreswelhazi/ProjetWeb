97e43e8f924bfd1db6d55fec4c365c413d982b0e
ayalaarif <larrifaya@gmail.com>
2023-11-10 22:26:35 +0100

    admin_redirection

--- admin/RedirectAdmin.php
+++ admin/RedirectAdmin.php
@@ -3,5 +3,10 @@
     if (!(isset($_SESSION['id'])))
     {
         header("Location:login.php");
+    }elseif ($_SESSION['role']=='client')
+    {
+        header("Location:../index.php");
     }
+
+    
 ?>
